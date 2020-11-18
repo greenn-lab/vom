c05a2fa4dddac59e3294d20667e3df63b46c8183
greenn-lab <greenn.lab@gmail.com>
2020-11-18 23:49:17 +0900

    add//frontend scaffolding

---
+++ vom-face/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-container fluid>
+      <router-view></router-view>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+.v-application {
+  font-weight: 300
+}
+</style>
